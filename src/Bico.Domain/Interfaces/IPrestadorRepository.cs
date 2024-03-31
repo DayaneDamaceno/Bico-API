aaebc16037e5d14612b076f665031f68e844bb61
Dayane Damaceno <Dayane.Damaceno@t-systems.com>
2024-03-31 13:12:52 -0300

    [FEAT] Add consulta por prestadores mais proximso

--- src/Bico.Domain/Interfaces/IPrestadorRepository.cs
+++ src/Bico.Domain/Interfaces/IPrestadorRepository.cs
@@ -1,5 +1,8 @@
-﻿namespace Bico.Domain.Interfaces;
+﻿using Bico.Domain.Entities;
+
+namespace Bico.Domain.Interfaces;
 
 public interface IPrestadorRepository
 {
+    Task<List<Prestador>> ObterPrestadoresMaisProximosAsync(int clienteId, int habilidadeId);
 }
--- src/Bico.Infra/Repositories/PrestadorRepository.cs
+++ src/Bico.Infra/Repositories/PrestadorRepository.cs
@@ -1,5 +1,8 @@
-﻿using Bico.Domain.Interfaces;
+﻿using Bico.Domain.Entities;
+using Bico.Domain.Interfaces;
 using Bico.Infra.DBContext;
+using Bico.Infra.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bico.Infra.Repositories;
 
@@ -10,5 +13,22 @@ public class PrestadorRepository : IPrestadorRepository
     public PrestadorRepository(BicoContext bicoContext)
     {
         _context = bicoContext;
+    }
+
+    public async Task<List<Prestador>> ObterPrestadoresMaisProximosAsync(int clienteId, int habilidadeId)
+    {
+        var localizacaoDoCliente = await _context.Clientes
+                             .Where(c => c.Id == clienteId)
+                             .Select(c => c.Localizacao)
+                             .FirstOrDefaultAsync();
+
+        var prestadoresProximos = _context.Prestadores
+            .Where(p => p.Habilidades.Any(h => h.Id == habilidadeId))
+            .Where(p => p.Localizacao.StDWithin(localizacaoDoCliente, p.RaioDeAlcance, true))
+            .OrderBy(p => p.Localizacao.StDistance(localizacaoDoCliente, true))
+            .ToList();      
+
+
+        return prestadoresProximos;
     }
 }
