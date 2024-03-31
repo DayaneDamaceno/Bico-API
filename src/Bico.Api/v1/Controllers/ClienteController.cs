972330e4805ff2514f289ed5fb4920390aec5f97
Dayane Damaceno <Dayane.Damaceno@t-systems.com>
2024-03-31 13:14:54 -0300

    [FEAT] Criando nova rota para buscar prestadores mais proximos de um cliente e que seja da habilidade selecionada

---
+++ src/Bico.Api/v1/Controllers/ClienteController.cs
@@ -0,0 +1,30 @@
+﻿using Asp.Versioning;
+using Bico.Api.v1.DTOs;
+using Bico.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bico.Api.v1.Controllers;
+
+[ApiVersion(1.0)]
+[ApiController]
+[Route("v{version:apiVersion}/clientes")]
+public class ClienteController : ControllerBase
+{
+    private readonly IPrestadorRepository _prestadorRepository;
+
+    public ClienteController(IPrestadorRepository prestadorRepository)
+    {
+        _prestadorRepository = prestadorRepository;
+    }
+
+    [HttpGet("{clienteId}/prestadores/proximos")]
+    public async Task<ActionResult> ObterPrestadoresMaisProximos(int clienteId, [FromQuery]int habilidade)
+    {
+        var prestadores = await _prestadorRepository.ObterPrestadoresMaisProximosAsync(clienteId, habilidade);
+        var prestadoresDto = prestadores.Select(p => new PrestadorDto(p)).ToList();
+
+        return Ok(prestadoresDto);
+    }
+
+
+}
---
+++ src/Bico.Api/v1/DTOs/PrestadorDto.cs
@@ -0,0 +1,24 @@
+﻿using Bico.Domain.Entities;
+
+namespace Bico.Api.v1.DTOs;
+
+public class PrestadorDto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string AvatarUrl { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+
+    //public IEnumerable<HabilidadeDto> Habilidades { get; set; }
+
+    public PrestadorDto(Prestador prestador)
+    {
+        Id = prestador.Id;
+        Nome = prestador.Nome;
+        Latitude = prestador.Localizacao?.Coordinate.Y ?? 0;
+        Longitude = prestador.Localizacao?.Coordinate.X ?? 0;
+        AvatarUrl = prestador.AvatarUrl;
+        //Habilidades = prestador.Habilidades.Select(x => new HabilidadeDto(x));
+    }
+}
