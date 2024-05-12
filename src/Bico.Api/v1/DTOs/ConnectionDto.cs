68a016806a8722918b82c397401944b9475ce368
Dayane Damaceno <Dayane.Damaceno@t-systems.com>
2024-05-12 19:29:47 -0300

    [FEAT] Hub WebSocket para enviar mensagens do server para o client

---
+++ src/Bico.Api/v1/DTOs/ConnectionDto.cs
@@ -0,0 +1,58 @@
+﻿namespace Bico.Api.v1.DTOs;
+
+public class ConnectionDto<T>
+{
+    private readonly Dictionary<T, HashSet<string>> _connections = [];
+
+    public int Count
+    {
+        get
+        {
+            return _connections.Count;
+        }
+    }
+
+    public void Add(T key, string connectionId)
+    {
+        lock (_connections)
+        {
+            if (!_connections.TryGetValue(key, out var connections))
+            {
+                connections = [];
+                _connections[key] = connections;
+            }
+
+            connections.Add(connectionId);
+        }
+    }
+
+    public void Remove(T key, string connectionId)
+    {
+        lock (_connections)
+        {
+            if (_connections.TryGetValue(key, out var connections))
+            {
+                connections.Remove(connectionId);
+
+                if (connections.Count == 0)
+                {
+                    _connections.Remove(key);
+                }
+            }
+        }
+    }
+
+    public IEnumerable<string> GetConnections(T key)
+    {
+        lock (_connections)
+        {
+            if (_connections.TryGetValue(key, out var connections))
+            {
+                return connections;
+            }
+
+            return [];
+        }
+    }
+}
+
